64cfc89eb4f103de0eb210e90bb82eb01c69fb9b
Fabio Santana <fpascoal@atech.com.br>
2018-07-13 16:12:01 -0300

    Configuration of Srping Cloud and Vault

--- catalog-service/src/main/java/com/lab/catalogservice/controllers/ProductController.java
+++ catalog-service/src/main/java/com/lab/catalogservice/controllers/ProductController.java
@@ -5,7 +5,6 @@ import com.lab.catalogservice.exceptions.ProductNotFoundException;
 import com.lab.catalogservice.services.ProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
